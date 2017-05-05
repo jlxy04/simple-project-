6448927bc74a9659aac9431c437636d2dc25159c
lijun <jlxy04@gmail.com>
2017-05-05 17:16:14 +0800

    update

---
+++ yiyao-dal/src/test/java/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `demo_user` (
+  `id` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `mobile` varchar(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `points` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
