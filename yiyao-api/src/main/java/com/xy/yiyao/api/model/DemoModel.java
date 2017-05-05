e32851ef5365b2e4abb068a3d7bc11348659cd54
lijun <jlxy04@gmail.com>
2017-05-05 10:22:07 +0800

    update

---
+++ yiyao-api/src/main/java/com/xy/yiyao/api/model/DemoModel.java
@@ -0,0 +1,69 @@
+package com.xy.yiyao.api.model;
+
+/**
+ * 测试entity
+ * @author Administrator
+ *
+ */
+public class DemoModel {
+	
+	private String id;
+	
+	private String name;
+	
+	private String password;
+	
+	private String mobile;
+	
+	private String address;
+	
+	private Integer points;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getPoints() {
+		return points;
+	}
+
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+}
---
+++ yiyao-api/src/main/java/com/xy/yiyao/api/service/DemoService.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.xy.yiyao.api.service;
+
+import com.xy.yiyao.api.model.DemoModel;
+
+/**
+ * @author Administrator
+ *
+ */
+public interface DemoService {
+	
+	/**
+	 * 新增
+	 * @param demoModel
+	 * @return
+	 */
+	public DemoModel addUser(DemoModel demoModel);
+	
+}
