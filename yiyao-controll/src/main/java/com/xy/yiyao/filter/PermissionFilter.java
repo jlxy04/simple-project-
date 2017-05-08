f6099e3e970013e7d7d60b9d63bba8420611219a
lijun <jlxy04@gmail.com>
2017-05-08 13:58:14 +0800

    update

---
+++ yiyao-controll/src/main/java/com/xy/yiyao/filter/PermissionFilter.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.xy.yiyao.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PermissionFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		System.out.println(httpServletRequest.getParameterMap());
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+}
